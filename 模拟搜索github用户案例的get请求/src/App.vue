fc67f420eaa22958120763d929a0e8f1f5d00556
张宇 <74807735+decTaipei@users.noreply.github.com>
2022-07-17 22:18:11 +0800

    模拟github搜索用户的get请求数据

---
+++ 模拟搜索github用户案例的get请求/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+   <!-- <Lists></Lists> -->
+   <Sousuo></Sousuo>
+  </div>
+</template>
+
+<script>
+// import Lists from '../src/views/Lists.vue'
+import Sousuo from '../src/views/Sousuo.vue'
+  export default {
+     components:{
+       Sousuo,
+      //  List
+     }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
