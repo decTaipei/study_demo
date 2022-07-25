5bd02ca074b9d0ae1eec2af0337a56c71d4885b7
张宇 <74807735+decTaipei@users.noreply.github.com>
2022-07-25 22:33:30 +0800

    Add files via upload
    vue插槽的使用

---
+++ vue插槽的使用/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Slot1 title="名字">
+      <template scope="aaaaa">
+        <!-- {{zhangyu.name}} -->
+        <ul>
+          <li v-for="(item, index) in aaaaa.mingzi" :key="index">
+            {{ item }}
+          </li>
+        </ul>
+      </template>
+    </Slot1>
+
+    <Slot1 title="名字">
+      <template scope="aaaaa">
+        <!-- {{zhangyu.name}} -->
+        <ol>
+          <li v-for="(item, index) in aaaaa.mingzi" :key="index">
+            {{ item }}
+          </li>
+        </ol>
+      </template>
+    </Slot1>
+  </div>
+</template>
+
+<script>
+import Slot1 from "../src/views/slot1.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: { Slot1 },
+};
+</script>
+
+<style lang="less" scoped></style>
---
+++ vue插槽的使用/src/views/slot1.vue
@@ -0,0 +1,22 @@
+<template>
+    <div> 
+        <h1>{{ title }}分类</h1> 
+         <slot :mingzi="mingzi"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                 mingzi: ["zy", "zyq", "cyq"],
+                 
+            }
+        },
+        props:['title']
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
